038c38c122f38691b83cd0c781143a461460639b
Federico Mestrone <239547+fmestrone@users.noreply.github.com>
2026-07-15 13:50:58 +0100

    PV and PVC variables

--- env-vars.sh
+++ env-vars.sh
@@ -12,6 +12,7 @@ export IP_RANGE_SVC=10.9.0.0/16
 export LWS_VERSION=v0.8.0
 
 # Derived variables
+export BASTION_VM_NAME=${APP_NAME}-bastion
 export VPC_NAME=${APP_NAME}-net
 export VPC_SUBNET=${VPC_NAME}-subnet-1
 export VPC_FW_INTERNAL=${VPC_NAME}-allow-internal
@@ -20,8 +21,11 @@ export CLUSTER_L4_NAME=${APP_NAME}-l4-cluster
 export BUCKET_NAME=${APP_NAME}-bucket
 export SVCACC_GSA=${APP_NAME}-gsa
 export SVCACC_KSA=${APP_NAME}-ksa
-export BASTION_VM_NAME=${APP_NAME}-bastion
+export GKE_PV_STORAGE=${APP_NAME}-gcs-pv
+export GKE_PVC_STORAGE=${APP_NAME}-gcs-pvc
 export LUSTRE_INSTANCE=${APP_NAME}-lustre
+export GKE_PV_LUSTRE=${APP_NAME}-lustre-pv
+export GKE_PVC_LUSTRE=${APP_NAME}-lustre-pvc
 
 if [ ! -f secrets/.hf_token ]; then
   echo "WARNING: secrets/.hf_token file does not exist!"
